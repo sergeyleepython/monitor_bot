152e61fa57a1e605acf69510a9db90d6d35d8a65
Sergey Lee <johndoe@example.com>
2018-07-31 12:24:54 +0300

    Try using job_queue

--- monitor_bot.py
+++ monitor_bot.py
@@ -2,15 +2,22 @@ from telegram.ext import Updater, CommandHandler
 
 from config import BOT_TOKEN, REQUEST_KWARGS, CHAT_ID, MONITORED_URL, SUBSTRING
 
+STARTING_MESSAGE = 'monitoring up'
 
-def hello(bot, update):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+
+def starting_notification(bot, job):
+    bot.send_message(chat_id=CHAT_ID, text=STARTING_MESSAGE)
+
+
+def callback_monitor_1min(bot, job):
+    bot.send_message(chat_id=CHAT_ID, text='One message every minute')
 
 
 updater = Updater(BOT_TOKEN, request_kwargs=REQUEST_KWARGS)
 
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
+j = updater.job_queue
+j.run_once(starting_notification, 0)
+j.run_repeating(callback_monitor_1min, interval=60, first=0)
 
 updater.start_polling()
 updater.idle()
